d86ed63a8595696ea1c430b807e03affce7a8362
pulquepara2 <veronika.morin@hotmail.com>
2023-03-08 12:28:19 +0100

    erledigt

--- LoopsTriangle.java
+++ LoopsTriangle.java
@@ -1,2 +1,12 @@
 public class LoopsTriangle {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 11; i++){
+            System.out.println();
+            for (int j = 0; j < i; j++  ){
+                System.out.print(j);
+            }
+        }
+
+    }
 }
