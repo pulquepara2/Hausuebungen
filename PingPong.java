0b1788b4ed7e000e5ca7329bebefb1907a73a297
pulquepara2 <veronika.morin@hotmail.com>
2023-03-08 11:57:15 +0100

    erledigt

--- PingPong.java
+++ PingPong.java
@@ -1,2 +1,17 @@
 public class PingPong {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 101) {
+            if (i % 6 == 0) {
+                System.out.println("ping");
+            } else if (i % 3 == 0) {
+                System.out.println("pong");
+            } else if (i % 2 == 0) {
+                System.out.println("pingpong");
+            } else {
+                System.out.println("-");
+            }
+            i++;
+        }
+    }
 }
